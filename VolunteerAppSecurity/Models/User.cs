97ed3755cf4e0455a23528dec734b6cd4b94ca1a
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-03-15 17:31:06 +0200

    changed User model

--- VolunteerAppSecurity/Models/User.cs
+++ VolunteerAppSecurity/Models/User.cs
@@ -5,5 +5,6 @@ namespace VolunteerAppSecurity.Models
     public class User : IdentityUser<Guid>
     {
         public string? RefreshToken { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
