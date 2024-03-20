14335e0ec338af48425e817a420b63d25cdbb6d9
Diana Nalyvayko <nalivaykodiana@gmail.com>
2024-03-20 22:56:49 +0200

    Added UserName to RegisterDTO

--- VolunteerAppSecurity/DTOs/RegisterDTO.cs
+++ VolunteerAppSecurity/DTOs/RegisterDTO.cs
@@ -3,6 +3,8 @@
     public class RegisterDTO
     {
         public string Email { get; set; }
+        public string UserName { get; set; }
+
         public string Password { get; set; }
         public string RoleName { get; set; }
     }
